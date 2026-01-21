a9ba9426b26b56ce1150128138fe1adab4c9608d
VIkas Kashyap <vikas965046@gmail.com>
2026-01-21 22:10:38 +0530

    Update FindDuplicateInArray.cpp

--- Array/Basic Array Operation Questions/FindDuplicateInArray.cpp
+++ Array/Basic Array Operation Questions/FindDuplicateInArray.cpp
@@ -3,6 +3,19 @@
 #include<algorithm>
 using namespace std;
 
-int duplicate(vector<int> nums){
-    
+int duplicate(vector<int> nums, int n){
+    sort(nums.begin(), nums.end());
+    for(int i = 1; i < n; i++){    
+        if(nums[i] == nums[i-1]){
+            return nums[i];
+        }
+    }
+    return -1;
+}
+
+int main(){
+    vector<int> nums = {1, 3, 4, 2, 2}; 
+    cout << "Duplicate is: " << duplicate(nums, nums.size());
+
+    return 0;
 }
