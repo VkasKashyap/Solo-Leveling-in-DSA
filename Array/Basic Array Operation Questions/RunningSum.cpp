313cb98b085f2c44e005565949be9d23e4ee0d6c
VIkas Kashyap <vikas965046@gmail.com>
2026-01-21 23:36:35 +0530

    Create RunningSum.cpp

---
+++ Array/Basic Array Operation Questions/RunningSum.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+vector<int> runningSum(vector<int>& nums) {
+    for(int i = 1; i < nums.size(); i++){
+        nums[i] += nums[i-1];
+    }
+    return nums;
+}
+
+int main() {
+    vector<int> nums = {1, 2, 3, 4};
+    vector<int> result = runningSum(nums);
+    cout << "Running Sum: ";
+    for(int x : result) {
+        cout << x << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
